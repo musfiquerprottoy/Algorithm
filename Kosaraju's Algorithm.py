b8926809b4bb65ba4b5fc403792559b1478aeba9
musfiquerprottoy <musfiquer36@gmail.com>
2025-08-29 19:12:44 +0600

    Kosaraju's Algorithm

--- Kosaraju's Algorithm.py
+++ Kosaraju's Algorithm.py
@@ -66,3 +66,13 @@ if __name__ == "__main__":
     print("\nStrongly Connected Components:")
     for comp in sccs:
         print(comp)
+'''
+Test case:
+Enter number of vertices and edges: 5 5
+Enter edges (u v):
+1 0
+0 2
+2 1
+0 3
+3 4
+'''
